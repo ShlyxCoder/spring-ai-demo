02e82d42127751713fe217872dfe1892e0823134
shelly1227 <40505282@qq.com>
2025-04-01 12:59:27 +0800

    feat:初始化项目工程

---
+++ src/main/java/com/shelly/ai/Application.java
@@ -0,0 +1,10 @@
+package com.shelly.ai;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        org.springframework.boot.SpringApplication.run(Application.class, args);
+    }
+}
